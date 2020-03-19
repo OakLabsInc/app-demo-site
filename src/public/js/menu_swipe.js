9a2822b0226aefa4905770e13a9192405eae0e66
Richard Joffray <rjoffray@richards-mbp.lan>
2020-03-19 12:52:15 -0700

    change to click to activate menu

--- src/public/js/menu_swipe.js
+++ src/public/js/menu_swipe.js
@@ -1,31 +1,40 @@
 
 var body = document.getElementsByTagName("body")[0]
 
-var swipeZone = document.createElement("div");
-// swipeZone.innerText = "Swipe Me"
-swipeZone.setAttribute("id", "swipe-zone")
-swipeZone.style.position = "absolute"
-swipeZone.style.left = "0"
-swipeZone.style.right = "0"
-swipeZone.style.top = "0"
-swipeZone.style.bottom = "0"
-swipeZone.zIndex = "10000"
+var hitZone = document.createElement("div");
+// hitZone.innerText = "Swipe Me"
+hitZone.setAttribute("id", "swipe-zone")
+hitZone.style.position = "absolute"
+hitZone.style.left = "0"
+hitZone.style.width = "100px"
+hitZone.style.height = "100px"
+// hitZone.style.right = "80%"
+hitZone.style.top = "0"
+// hitZone.style.bottom = "90%"
+hitZone.zIndex = "10000"
 
-// swipeZone.style.border = "5px solid yellow"
-swipeZone.style.color = "#ffffff"
-body.appendChild(swipeZone);
+hitZone.style.border = "5px solid yellow"
+hitZone.style.color = "#ffffff"
+body.appendChild(hitZone);
 
-var listener = SwipeListener(swipeZone)
-swipeZone.addEventListener('swipe', function (e) {
+// var listener = SwipeListener(hitZone)
+// hitZone.addEventListener('swipe', function (e) {
     
-    let results = e.detail
-    if(results.directions.right){
-        console.log("Swipe for menu", results.directions.x)
+//     let results = e.detail
+//     if(results.directions.right){
+//         console.log("Swipe for menu", results.directions.x)
+//         var xhr = new XMLHttpRequest();
+//         xhr.onreadystatechange = function() {};
+//         xhr.open('GET', 'http://localhost:9000/focus');
+//         xhr.send()
+//     }
+// })
+
+hitZone.addEventListener('click', function (e) {
+
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {};
         xhr.open('GET', 'http://localhost:9000/focus');
         xhr.send()
-    }
+    
 })
-
-
